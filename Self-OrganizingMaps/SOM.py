179c6d123447ecb2e836ab514e217319c1c85d1c
sa06840 <sa06840@st.habib.edu.pk>
2023-03-29 00:23:28 +0500

    updated get best cluster function

--- Self-OrganizingMaps/SOM.py
+++ Self-OrganizingMaps/SOM.py
@@ -1,3 +1,4 @@
+from math import ceil, sqrt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import random
@@ -5,22 +6,24 @@ import random
 
 class SOM():
 
-    def __init__(self, filename, numClusters, learningRate) -> None:
+    def __init__(self, filename, learningRate) -> None:
         self.fileName = filename
         self.readFile()
+        self.replaceNan()
         self.removeUniqueColumns()
         self.oneHotEncoding()
         self.scaleData()
         self.data = self.df.values.tolist()
+        # print(self.data)
         self.inputVectors = len(self.data[0])
-        self.numClusters = numClusters
+        self.getGridSize()
         self.weights = []
         self.learningRate = learningRate
         self.weightsDifference = []
     
     def readFile(self):
         df = pd.read_csv(self.fileName)
-        self.df = df.dropna()
+        self.df = df
     
     def removeUniqueColumns(self):
         numberOfUnique = self.df.nunique()
@@ -30,7 +33,15 @@ class SOM():
             if numberOfUnique[i] == len(self.df) and not(self.df[columnName].dtype.kind in 'iufcb'):
                 uniqueColumns.append(columnName)
         self.df.drop(uniqueColumns, axis=1, inplace=True)
-        
+
+    def replaceNan(self):
+        columnsWithNan = self.df.columns[self.df.isna().any()].tolist()
+        for columnName in columnsWithNan:
+            avg = self.df[columnName].mean()
+            self.df[columnName] = self.df[columnName].fillna(avg)
+
+        # self.df.to_csv("newfile.csv", index=False)
+          
     def oneHotEncoding(self):
         categoricalColumns = self.df.select_dtypes(include=["object"])
         categoricalColumnsLst = []
@@ -38,51 +49,101 @@ class SOM():
             categoricalColumnsLst.append(col)
         self.df = pd.get_dummies(self.df, columns = categoricalColumnsLst)
 
+    def getGridSize(self):
+        self.numClusters = 5*(sqrt((len(self.df))))
+        self.gridSize = ceil(sqrt(self.numClusters))
+
     def scaleData(self):
         scaler = MinMaxScaler()
         df = self.df.copy()
         self.df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
 
     def initializeWeights(self):
-        for row in range(self.inputVectors):
-            weights = []
-            for column in range(self.numClusters):
-                weight = random.random()
-                weights.append(weight)
-            self.weights.append(weights)
-        # print(self.weights)
+        for y in range(self.gridSize):
+            row = []
+            for x in range(self.gridSize):
+                weights = []
+                for i in range(self.inputVectors):
+                    weight = random.random()
+                    weights.append(weight)
+                row.append(weights)
+            self.weights.append(row)
 
-    def calculateDistances(self, inputVector):
-        distances = []
-        for column in range(self.numClusters):
-            summation = 0
-            for row in range(self.inputVectors):
-                value = (inputVector[row] - self.weights[row][column])**2
-                summation += value
-            distances.append(summation)
-        # print(distances)
-        return distances
+        # print(self.weights)
+        # print(len(self.weights))
+        # for row in range(self.inputVectors):
+        #     weights = []
+        #     for column in range(self.numClusters):
+        #         weight = random.random()
+        #         weights.append(weight)
+            
+        #     self.weights.append(weights)
+        # # print(self.weights)
 
     def selectBestCluster(self, inputVector):
-        distances = self.calculateDistances(inputVector)
-        minDistance = min(distances)
-        bestClusterIndex = distances.index(minDistance)
-        # print(bestClusterIndex)
-        return bestClusterIndex
+        # distances = []
+        minimum = [0,0]
+        minimumDistance = 10000000000000
+        for y in range(self.gridSize):
+            for x in range(self.gridSize):
+                weightList = self.weights[y][x]
+                distance = 0
+                for index in range(self.inputVectors):
+                    value = (inputVector[index] - weightList[index])**2
+                    distance += value
+                if (distance < minimumDistance):
+                    minimum[0] = y
+                    minimum[1] = x
+                    minimumDistance = distance
+
+        return minimum
+    
+                # distances.append(distance)
+
+        # distances = []
+        # for column in range(self.numClusters):
+        #     summation = 0
+        #     for row in range(self.inputVectors):
+        #         value = (inputVector[row] - self.weights[row][column])**2
+        #         summation += value
+        #     distances.append(summation)
+        # print(distances)
+        # return distances
+
+    # def selectBestCluster(self, inputVector):
+    #     distances = self.calculateDistances(inputVector)
+    #     minDistance = min(distances)
+    #     bestClusterIndex = distances.index(minDistance)
+    #     # print(bestClusterIndex)
+    #     return bestClusterIndex
+
+
+# IMPLEMENT NEIGHBOURHOOD FUNCTION
 
     def updateWeights(self, inputVector, bestClusterIndex):
-        for row in range(self.inputVectors):
-            oldWeightValue = self.weights[row][bestClusterIndex]
-            newWeightValue = oldWeightValue + self.learningRate*(inputVector[row]-oldWeightValue)
+        # y = bestClusterIndex//self.gridSize    #row
+        # x = bestClusterIndex % self.gridSize   #column
+        y = bestClusterIndex[0]
+        x = bestClusterIndex[1]
+        for weightIndex in range(self.inputVectors):
+            oldWeightValue = self.weights[y][x][weightIndex]
+            newWeightValue = oldWeightValue + self.learningRate*(inputVector[weightIndex]-oldWeightValue)
             difference = newWeightValue-oldWeightValue
             self.weightsDifference.append(difference)
-            self.weights[row][bestClusterIndex] = newWeightValue
+            self.weights[y][x][weightIndex] = newWeightValue
+
+        # for row in range(self.inputVectors):
+        #     oldWeightValue = self.weights[row][bestClusterIndex]
+        #     newWeightValue = oldWeightValue + self.learningRate*(inputVector[row]-oldWeightValue)
+        #     difference = newWeightValue-oldWeightValue
+        #     self.weightsDifference.append(difference)
+        #     self.weights[row][bestClusterIndex] = newWeightValue
         
 
 
 
-def SelfOrganizingMaps(filename, numClusters, learningRate, iterations):
-    s = SOM(filename, numClusters, learningRate)
+def SelfOrganizingMaps(filename, learningRate, iterations):
+    s = SOM(filename, learningRate)
     s.initializeWeights()
 
     for iteration in range(iterations):
@@ -103,19 +164,19 @@ def SelfOrganizingMaps(filename, numClusters, learningRate, iterations):
 
 
 filename = 'Self-OrganizingMaps/worldometer_coronavirus_summary_data.csv'
-numClusters = 2
 learningRate = 0.5
 iterations = 100
 
-SelfOrganizingMaps(filename, numClusters, learningRate, iterations)
+SelfOrganizingMaps(filename, learningRate, iterations)
 
 
 # filename = 'Self-OrganizingMaps/worldometer_coronavirus_summary_data.csv'
 # numClusters = 2
 # inputVector = [0.0020963644071506204,0.007489443770728929,0.0019951958144534114,0.004823212496814616,0.13502464831068897,0.006058385453058164,0.034425319267073845,0.000930515331706835,0.0008408518256701044,0.028176465530080016,0.0,1.0,0.0,0.0,0.0,0.0]
 # learningRate = 0.5
-# s = SOM(filename, numClusters, learningRate)
+# s = SOM(filename, learningRate)
 # s.initializeWeights()
+# s.calculateDistances(inputVector)
 # b = s.selectBestCluster(inputVector)
 # s.updateWeights(inputVector,b)
 # print(s.weights)
