61b8069a31d5174db18951dc8348506284d21ae0
sa06840 <sa06840@st.habib.edu.pk>
2023-04-02 16:31:41 +0500

    final

--- Self-OrganizingMaps/SOM.py
+++ Self-OrganizingMaps/SOM.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 class SOM():
 
+    # initializes all the datasets and variables
     def __init__(self, filename, learningRate, iterations) -> None:
         self.fileName = filename
         self.readFile()
@@ -26,10 +27,12 @@ class SOM():
         self.timeConstant = log(iterations/self.radius)
         self.IDstoColor = []
 
+    # reads the csv and stores the data in a dataframe
     def readFile(self):
         df = pd.read_csv(self.fileName)
         self.df = df
     
+    # removes non-numerical columns that have unique entries but stores the iD column in a list to be used for mapping on the world map
     def removeUniqueColumns(self):
         numberOfUnique = self.df.nunique()
         uniqueColumns = []
@@ -45,12 +48,14 @@ class SOM():
         self.uniquesIDs = self.df[iDcolumn].values.tolist() 
         self.df.drop(uniqueColumns, axis=1, inplace=True)
 
+    # replaces the Nan values with the average of the values of the column in which the Nan is present
     def replaceNan(self):
         columnsWithNan = self.df.columns[self.df.isna().any()].tolist()
         for columnName in columnsWithNan:
             avg = self.df[columnName].mean()
             self.df[columnName] = self.df[columnName].fillna(avg)
-          
+
+    # one hot encodes all the columns that consist of categroical data. Drops the "continents" column since it yeilds better results         
     def oneHotEncoding(self):
         categoricalColumns = self.df.select_dtypes(include=["object"])
         categoricalColumnsLst = []
@@ -62,15 +67,18 @@ class SOM():
         if len(categoricalColumnsLst) != 0:
             self.df = pd.get_dummies(self.df, columns = categoricalColumnsLst)
 
+    # uses the size of the input data to get the grid size of the neurons grid
     def getGridSize(self):
         self.numClusters = 5*(sqrt((len(self.df))))
         self.gridSize = ceil(sqrt(self.numClusters))
 
+    # sclaes numeric data to values between 0 and 1
     def scaleData(self):
         scaler = MinMaxScaler()
         df = self.df.copy()
         self.df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
-
+    
+    # randomly initializes all the weights of all the neurons in the neuron grid
     def initializeWeights(self):
         for y in range(self.gridSize):
             row = []
@@ -82,6 +90,7 @@ class SOM():
                 row.append(weights)
             self.weights.append(row)
 
+    # returns the coordinates of the closest neuron (BMU) for the input vector provided
     def selectBestCluster(self, inputVector):
         minimum = [0,0]
         minimumDistance = 10000000000000
@@ -98,21 +107,25 @@ class SOM():
                     minimumDistance = distance
         return minimum
     
+    # updates the radius using the current iteration number
     def updateRadius(self, iterationNumber):
         newRadius = self.radius * exp(-(iterationNumber/self.timeConstant))
         self.radius = newRadius
 
+    # caculates the neighbourhood value that is used to calculate the weights of the neighbours of the BMU
     def neighbourhoodFunction(self, distance):
         num = distance**2
         den = 2*(self.radius**2)
         value = exp(-(num/den))
         return value
 
+    # returns the distance betweeen the BMU and the provided neuron's weight list
     def getEuclideanDistance(self, bestClusterIndex, weightListCoordinates):
         value = (weightListCoordinates[0]-bestClusterIndex[0])**2 + (weightListCoordinates[1]-bestClusterIndex[1])**2
         distance = sqrt(value)
         return distance
 
+    # updates the weights of the BMU and its neighbouring neurons
     def updateWeights(self, inputVector, bestClusterIndex):
         rowBestCluster = bestClusterIndex[0]
         columnBestCluster = bestClusterIndex[1]
@@ -142,6 +155,7 @@ class SOM():
                             weightList[weightIndex] = newWeightValue
                         self.weights[row][column] = weightList
 
+    # assigns every ID its corresponding neuron that it is closest to after all the weights have been optimised 
     def assignCoordinatesID(self):
         xIndex = 0
         for x in self.data:
@@ -149,7 +163,8 @@ class SOM():
             iD = self.uniquesIDs[xIndex]
             self.IDstoColor.append([iD, bestClusterCoordinates])
             xIndex+=1
-
+    
+    # constructs the coloured grid from the optimised weights and also assigns each ID the coloured patch which it belongs to. The coloured grid is then displayed
     def getColorGridfromWeights(self):
         figure = plt.figure(figsize=(self.gridSize, self.gridSize))
         ax = figure.add_subplot(111, aspect='equal')
@@ -181,7 +196,8 @@ class SOM():
                         lst.append(colour)
                 ax.text(row+0.5, column+0.5, label, ha='center', va='center', color="white", fontsize=5)
         plt.show()
-                    
+    
+    # uses geopandas and the assigned colour to every individual ID (country) to construct a coloured world map which is then displayed
     def getWorldMap(self):
         world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
         fig, ax = plt.subplots(figsize=(10, 10))
@@ -195,7 +211,7 @@ class SOM():
 
         
 
-
+# consists of the training loop that updates the weights and stops once the weight difference for all the weights becomes negligible
 def SelfOrganizingMaps(filename, learningRate, iterations):
     s = SOM(filename, learningRate, iterations)
     s.initializeWeights()
@@ -205,7 +221,6 @@ def SelfOrganizingMaps(filename, learningRate, iterations):
         for x in s.data:
             bestMatchingIndex = s.selectBestCluster(x)
             s.updateWeights(x, bestMatchingIndex)
-        print(bestMatchingIndex)
             
         if (all(w <= 0.00000000001 for w in s.weightsDifference)):
             break
@@ -219,12 +234,13 @@ def SelfOrganizingMaps(filename, learningRate, iterations):
     s.getColorGridfromWeights()
     s.getWorldMap()
 
-
+# un-comment the dataset which you want to use
 # filename  = 'Self-OrganizingMaps/datasets/worldCoronaVirusData.csv'
 # filename  = 'Self-OrganizingMaps/datasets/worldEnvironmentalData.csv'
 # filename  = 'Self-OrganizingMaps/datasets/worldHappinessData.csv'
 # filename  = 'Self-OrganizingMaps/datasets/worldPopulationData.csv'
 filename  = 'Self-OrganizingMaps/datasets/worldVaccinationData.csv'
+
 learningRate = 0.5
 iterations = 100
 
